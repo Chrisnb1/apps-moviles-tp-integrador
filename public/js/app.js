ede9352e4c129e476db4da8d32912851ff767f7b
Nicolas <acu.nicolas.1998@gmail.com>
2021-10-07 17:47:32 -0300

    header + navbar responsive

--- public/js/app.js
+++ public/js/app.js
@@ -94,4 +94,17 @@ const getXCategory = (category) => {
 }
 */
 
+// Menu navbar
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+
+function toggleMenu() {
+    menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
 
